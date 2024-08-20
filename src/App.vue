c9939cf0dfabf351e39db6cd655bf84f4c7c1c86
lvdameng <17721786639@163.com>
2024-08-20 18:24:37 +0800

    导入element组件库

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
   <div id="app">
+    <el-button type="primary" icon="el-icon-edit"></el-button>
+<el-button type="primary" icon="el-icon-share"></el-button>
+<el-button type="primary" icon="el-icon-delete"></el-button>
+<el-button type="primary" icon="el-icon-search">搜索</el-button>
+<el-button type="primary">上传<i class="el-icon-upload el-icon--right"></i></el-button>
   </div>
 </template>
-
-<style lang="less" scoped>
-</style>
+<style lang="less" scoped></style>
+<script>
+</script>
