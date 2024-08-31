ca8e70a4d1d281f404eda5d215c9fbc4617588e6
lvdameng <17721786639@163.com>
2024-08-31 22:21:44 +0800

    打包-dist文件瘦身和cdn地址引入

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? './' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
+  configureWebpack: {
+    externals: {
+      vue: 'Vue',
+      axios: 'axios',
+      dayjs: 'dayjs',
+      echarts: 'echarts',
+      'element-ui': 'ELEMENT',
+      'vue-quill-editor': 'VueQuillEditor',
+      'vue-router': 'VueRouter',
+      vuex: 'Vuex'
+    }
+  }
 })
